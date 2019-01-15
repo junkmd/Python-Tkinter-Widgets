a6a9951824af3ef151bee23d84f0830168af013d
junkmd <jun.k.job.finding12@gmail.com>
2019-01-15 23:49:34 +0900

    Added scrollable Frame widget.

---
+++ scrollable.py
@@ -0,0 +1,32 @@
+"""Tkinter modified Frame widgets inherited XView and/or YView."""
+import tkinter as tk
+from tkinter import ttk
+
+
+class ScrollableOuter(tk.Frame):
+    """Custom frame widget.
+    This will be parent of widget inherited XView and/or YView"""
+    def __init__(self, master, scroll=None, cnf={}, **kw):
+        tk.Frame.__init__(self, master)
+        if scroll is None:
+            scroll = tk.X + tk.Y
+        self.__x_scr, self.__y_scr, self.__interior = None, None, None
+        if tk.Y in scroll:
+            self.__y_scr = tk.Scrollbar(self, orient=tk.VERTICAL)
+            self.__y_scr.grid(row=0, column=1, sticky=tk.NW+tk.S)
+        if tk.X in scroll:
+            self.__x_scr = tk.Scrollbar(self, orient=tk.HORIZONTAL)
+            self.__x_scr.grid(row=1, column=0, sticky=tk.E+tk.W)
+        tk.Frame.config(self, cnf, **kw)
+
+    def set_interior(self, widget):
+        self.__interior = widget
+        if self.__x_scr is not None:
+            widget.configure(xscrollcommand=self.__x_scr.set)
+            self.__x_scr.config(command=widget.xview)
+        if self.__y_scr is not None:
+            widget.configure(yscrollcommand=self.__y_scr.set)
+            self.__y_scr.config(command=widget.yview)
+        widget.grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+
+    interior = property(fget=lambda self: self.__interior)
